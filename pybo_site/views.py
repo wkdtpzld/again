18632193c1144773116a2ee2b88306116567271c
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2021-09-11 21:17:14 +0900

    에러코드 수정

--- pybo_site/views.py
+++ pybo_site/views.py
@@ -242,7 +242,7 @@ def comment_delete_answer(request, comment_id):
 @login_required(login_url='common:login')
 def vote_question(request,question_id):
     question = get_object_or_404(Question, pk=question_id)
-    if request.user != question.author:
+    if request.user == question.author:
         messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
     else:
         question.voter.add(request.user)
@@ -251,7 +251,7 @@ def vote_question(request,question_id):
 def vote_answer(request, answer_id):
     answer = get_object_or_404(Answer, pk=answer_id)
 
-    if request.user != answer.author:
+    if request.user == answer.author:
         messages.error(request, '수정권한이 없습니다.')
     else:
         answer.voter.add(request.user)
