ca59992e5a16eee23af6bd65179d5fa422f08848
slwhswk <87063105+wkdtpzld@users.noreply.github.com>
2021-10-11 02:44:35 +0900

    freeze

--- common/urls.py
+++ common/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate')
+    path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name='common/password_reset.html'),name='password_reset'),
+    path('password_reset_done/',auth_views.PasswordResetDoneView.as_view(template_name='common/password_reset_done.html'),name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='common/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
--- pybo/settings.py
+++ pybo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mrxc&45aieu*kstomm!_ahr=puom+a!a3$m#)cq2vht6734_y4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['15.164.71.197']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'pybo_site',
     'common',
 ]
@@ -134,3 +135,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL ='/'
 
 LOGOUT_REDIRECT_URL ='/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+# 메일을 호스트하는 서버
+EMAIL_PORT = '587'
+# gmail과의 통신하는 포트
+EMAIL_HOST_USER = 'slwhswk9@gmail.com'
+# 발신할 이메일
+EMAIL_HOST_PASSWORD = 'jhi5384zqe'
+# 발신할 메일의 비밀번호
+EMAIL_USE_TLS = True
+# TLS 보안 방법
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# 사이트와 관련한 자동응답을 받을 이메일 주소,'webmaster@localhost'
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
